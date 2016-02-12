6273578518d3957305208e7fba3d3359b5ba4aec
d-m-wilson <t00lfan@yahoo.com>
2016-02-12 16:16:06 -0600

    Fixed missing carriage-return characters in generated scripts.

--- StaticGeneratorCommon/Globals.cs
+++ StaticGeneratorCommon/Globals.cs
@@ -283,7 +283,7 @@ namespace StaticGeneratorCommon
                     strNullJoin += "tmp.[" + strKey + "] IS NULL AND ";
                 }
                 strNullJoin = strNullJoin.Substring(0, strNullJoin.Length - 5);
-                string strDelete = string.Format("\tDELETE FROM {0} FROM {0} LiveTable\n\tLEFT JOIN @tblTempTable tmp ON {1}\n\tWHERE {2}", pstrTableName, strJoinClause, strNullJoin);
+                string strDelete = string.Format("\tDELETE FROM {0} FROM {0} LiveTable\r\n\tLEFT JOIN @tblTempTable tmp ON {1}\r\n\tWHERE {2}", pstrTableName, strJoinClause, strNullJoin);
 
                 // Format the provided template with our output
                 string strOutputScript = string.Format(pstrTemplate, strTableDef, strTmpInsert, strLiveInsert, strUpdate, strDelete);
